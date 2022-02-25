ab05b6f99388e361e1c08ed036c9eb6b19747c40
abu-sufian-opu <sufian.jnu@gmail.com>
2022-02-25 21:12:46 +0600

    upload all files

---
+++ app.js
@@ -0,0 +1,5 @@
+
+let student = new Student();
+let teacher = new Teacher('Abu Sufian', 34);
+
+console.log(teacher);
---
+++ src/Teacher.js
@@ -0,0 +1,22 @@
+
+class Teacher {
+
+    constructor(name, age) {
+
+        this.name = name;
+        this.age = age;
+    }
+
+    ageCal(){
+        return "I am 30 years old";
+    }
+
+    BMI(){
+        return "I am from BMI";
+    }
+
+    teacherInfo(){
+        return "I am Abu Sufian";
+    }
+
+}
